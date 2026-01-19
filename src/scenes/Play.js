41ccd6a56f594f5052169b791b3a7a65dbc193f1
Sophia Rainiel Arevalo Aguado <aguadosophiarainiel@gmail.com>
2026-01-18 21:53:55 -0800

    The Rocket Class + Fixed Rocket Sprite

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,7 @@ class Play extends Phaser.Scene{
     }
 
     create(){
+        // Background
         // Tile Sprite
         this.starfield = this.add.tileSprite(0, 0, 640, 480, "starfield") //places tile sprite
             .setOrigin(0, 0)
@@ -21,6 +22,12 @@ class Play extends Phaser.Scene{
             0xFFFFFF).setOrigin(0, 0)
         this.add.rectangle(game.config.width - borderUISize, 0, 
             borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0)
+
+        // Objects
+        // add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, 
+            game.config.height - borderUISize - borderPadding, "rocket")
+            .setOrigin(0.5, 0)
     }
 
     update(){
